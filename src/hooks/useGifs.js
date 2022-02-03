7feeb90ab020dbc7ba77d6836751f690913da7d7
TheFluvion <gaxton45@gmail.com>
2022-02-03 00:45:38 -0300

    New hook aplication

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,15 +2,20 @@ import {useContext, useEffect, useState } from "react"
 import getGifs from "../services/getGifs"
 import GifContext from '../Context/GifsContext'
 
+const INITAL_PAGE = 0
+
 export function useGifs({ keyword } = { keyword: null }) {
     const [loading, setLoading] = useState(false)
+    const [loadingNextPage, setLoadingNextPage] = useState(false)
+    const [page, setPage] = useState(INITAL_PAGE)
     const {gifs, setGifs} = useContext(GifContext)
+
+    //recuperamos la keyword del localStorage
+    const keywordToUse = keyword || localStorage.getItem
+    ('lastKeyword') || 'random'
 
     useEffect(function () {
         setLoading(true)
-        //recuperamos la keyword del localStorage
-        const keywordToUse = keyword || localStorage.getItem
-            ('lastKeyword') || 'random'
 
         getGifs({ keyword: keywordToUse })
             .then(gifs => {
@@ -19,9 +24,21 @@ export function useGifs({ keyword } = { keyword: null }) {
                 //guardamos la keyword en el localStorage
                 localStorage.setItem('lastKeyword', keyword)
             })
-    }, [keyword, setGifs])
+    }, [keyword, keywordToUse, setGifs])
 
-    return { loading, gifs }
+    useEffect (function () {
+        if (page === INITAL_PAGE) return
+
+        setLoadingNextPage(true)
+        getGifs({ keyword: keywordToUse, page})
+        .then(nextGifs => {
+            setGifs( prevGifs => prevGifs.concat(nextGifs))
+            setLoadingNextPage(false)
+    })
+    }, [keywordToUse, page, setGifs])
+
+
+    return { loading, loadingNextPage, gifs, setPage }
 }
 
 /*
