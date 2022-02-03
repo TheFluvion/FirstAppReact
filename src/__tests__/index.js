b28f507e26612b749b7607a7e7461c06d2026c9f
TheFluvion <gaxton45@gmail.com>
2022-02-03 17:43:40 -0300

    spinner edit

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitForElement } from '@testing-library/react';
 import App from '../App';
 
 //https://testing-library.com/docs/dom-testing-library/cheatsheet
 
-test('home work as expected', () => {
-  
+test('home work as expected', async () => {
+    const {container} = render (<App />)
+    const gifLink = await waitForElement( 
+        () => container.querySelector('.Gif-link')
+    )
+
+        expect(gifLink).toBeVisible()
 });
+
+test('search from could be used', async () =>{
+    render(<App></App>) 
+    const input = await screen.findByRole('textbox')
+    const button = await screen.findByRole('button')
+
+    fireEvent.change(input, {target: {value: 'Matrix'}})
+    fireEvent.click(button)
+
+    const title = await screen.findByText('Matrix')
+    expect(title).toBeVisible()
+})
--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './style.css'
+import image from './../../logo192.png'
+
 export default function Spinner(){
-    return <div className="ring"><div><div><div>Cargando🎅</div></div></div></div>
+    return <div className="ring">
+        <img className='spinner' alt='im-gif' src={image}></img>
+    </div>
 }
